b7d307df199f5ce731a66b56b1896969d9a534e5
Linda Hellborg <linda.hellborg@gmail.com>
2016-03-01 14:50:11 +0100

    effective onStoryData

--- scripts/app.js
+++ scripts/app.js
@@ -65,24 +65,17 @@ APP.Main = (function() {
 
     // This seems odd. Surely we could just select the story
     // directly rather than looping through all of them.
-    var storyElements = document.querySelectorAll('.story');
+    var story = document.getElementById('s-' + key);;
 
-    for (var i = 0; i < storyElements.length; i++) {
-
-      if (storyElements[i].getAttribute('id') === 's-' + key) {
-
-        details.time *= 1000;
-        var story = storyElements[i];
-        var html = storyTemplate(details);
-        story.innerHTML = html;
-        story.addEventListener('click', onStoryClick.bind(this, details));
-        story.classList.add('clickable');
+      details.time *= 1000;
+      var html = storyTemplate(details);
+      story.innerHTML = html;
+      story.addEventListener('click', onStoryClick.bind(this, details));
+      story.classList.add('clickable');
 
         // Tick down. When zero we can batch in the next load.
         storyLoadCount--;
 
-      }
-    }
 
     // Colorize on complete.
     if (storyLoadCount === 0)
@@ -302,7 +295,7 @@ APP.Main = (function() {
     var scrollTopCapped = Math.min(70, main.scrollTop);
     var scaleString = 'scale(' + (1 - (scrollTopCapped / 300)) + ')';
 
-    colorizeAndScaleStories();
+    //colorizeAndScaleStories();
 
     header.style.height = (156 - scrollTopCapped) + 'px';
     headerTitles.style.webkitTransform = scaleString;
