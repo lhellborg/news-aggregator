62f4bb00c083d45a679dd36b9f130638f8c1519a
Linda Hellborg <linda.hellborg@gmail.com>
2016-03-02 14:10:00 +0100

    remove js show and hide add css for show and hide stories

--- scripts/app.js
+++ scripts/app.js
@@ -83,19 +83,18 @@ APP.Main = (function() {
      requestAnimationFrame(onStoryData);
   }
 
+  var storyDetails = document.createElement('section');
+  storyDetails.classList.add('story-details');
+  document.body.appendChild(storyDetails);
+
   function onStoryClick(details) {
-
-    var storyDetails = $('sd-' + details.id);
-
-    // Wait a little time then show the story details.
-    setTimeout(showStory.bind(this, details.id), 60);
 
     // Create and append the story. A visual change...
     // perhaps that should be in a requestAnimationFrame?
     // And maybe, since they're all the same, I don't
     // need to make a new element every single time? I mean,
     // it inflates the DOM and I can only see one at once.
-    if (!storyDetails) {
+
 
       if (details.url)
         details.urlobj = new URL(details.url);
@@ -111,12 +110,9 @@ APP.Main = (function() {
         by: '', text: 'Loading comment...'
       });
 
-      //storyDetails = document.createElement('section');
       storyDetails.setAttribute('id', 'sd-' + details.id);
-      // storyDetails.classList.add('story-details');
       storyDetails.innerHTML = storyDetailsHtml;
 
-      // document.body.appendChild(storyDetails);
 
       commentsElement = storyDetails.querySelector('.js-comments');
       storyHeader = storyDetails.querySelector('.js-header');
@@ -151,98 +147,27 @@ APP.Main = (function() {
               localeData);
         });
       }
-    }
+
+    showStory(details.id);
 
   }
 
   function showStory(id) {
 
-    if (inDetails)
+    if (!storyDetails) {
       return;
-
-    inDetails = true;
-
-    var storyDetails = $('#sd-' + id);
-    var left = null;
-
-    if (!storyDetails)
-      return;
-
-    document.body.classList.add('details-active');
-    storyDetails.style.opacity = 1;
-
-    function animate () {
-
-      // Now figure out where it needs to go.
-      left += (0 - left) * 0.1;
-
-      // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left) > 0.5)
-      {
-        requestAnimationFrame(animate);
-      }
-      else
-      {
-        left = 0;
-      }
-
-      // And update the styles. Wait, is this a read-write cycle?
-      // I hope I don't trigger a forced synchronous layout!
-      storyDetails.style.left = left + 'px';
     }
+    else {
+     storyDetails.classList.add("visible");
+   }
+ };
 
-    // We want slick, right, so let's do a setTimeout
-    // every few milliseconds. That's going to keep
-    // it all tight. Or maybe we're doing visual changes
-    // and they should be in a requestAnimationFrame
 
-      // Find out where it currently is.
-    var storyDetailsPosition = storyDetails.getBoundingClientRect();
-    left = storyDetailsPosition.left;
-
-    requestAnimationFrame(animate);
-  }
 
   function hideStory(id) {
 
-    if (!inDetails)
-      return;
-
-    var storyDetails = $('#sd-' + id);
-    var left = 0;
-
-    document.body.classList.remove('details-active');
-    storyDetails.style.opacity = 0;
-
-    function animate () {
-
-      // Now figure out where it needs to go.
-      left += (target - left) * 0.1;
-
-      // Set up the next bit of the animation if there is more to do.
-      if (Math.abs(left - target) > 0.5) {
-        requestAnimationFrame(animate);
-      } else {
-        left = target;
-        inDetails = false;
-      }
-
-      // And update the styles. Wait, is this a read-write cycle?
-      // I hope I don't trigger a forced synchronous layout!
-      storyDetails.style.left = left + 'px';
-    }
-
-
-    // Find out where it currently is.
-    var mainPosition = main.getBoundingClientRect();
-    var target = mainPosition.width + 100;
-    // We want slick, right, so let's do a setTimeout
-    // every few milliseconds. That's going to keep
-    // it all tight. Or maybe we're doing visual changes
-    // and they should be in a requestAnimationFrame
-    requestAnimationFrame(animate);
+    storyDetails.classList.remove("visible");
   }
-
 
   main.addEventListener('scroll', function() {
 
